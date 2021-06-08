02965eba5ff309267ad6cbc43c0f8858a4c66e76
YoshikawaMao <lapinnoir2568stella@gmail.com>
2021-06-08 08:44:06 +0000

    [Finish]2-7

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+
+Refile.secret_key = '40707278606e34fb363ed7d43959a9b63ccfba21b4b73b51248580b1da487b42e65c6032180ecae941de0259366975f1dc4fc5d943a062461b84da320d66fca2'
